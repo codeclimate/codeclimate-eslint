89ae27e0013f8ce737b9acd5e88a932e00758901
Devon Blandin <dblandin@gmail.com>
2017-03-14 21:32:10 -0400

    Update fingerprint for complexity issues targeting anonymous functions
    As of https://github.com/eslint/eslint/pull/8058, the message for
    complexity violations targeting anonymous functions adhere to the
    following format:
    
    > Function has a complexity of {{complexity}}.
    
    This commit updates the fingerprint generated for complexity issues to
    address this variation. Unfortunately, because the function is
    anonymous, a name can't be included within the fingerprint calculation.
    
    This means that any anonymous function within the same file will receive
    the same fingerprint and if that fingerprint is ignored, all complexity
    issues for anonymous functions in the file will be ignored.

--- test/compute_fingerprint_test.js
+++ test/compute_fingerprint_test.js
@@ -12,6 +12,16 @@ describe("computeFingerprint", function() {
     expect(fingerprint).to.be.eq("449efcc6d162550a2b7f54eb0fdfaccd");
   });
 
+  it("handles complexity issues for anonymous functions", function() {
+    var path = "path.js"
+      , rule = "complexity"
+      , message = "Function has a complexity of 10";
+
+    var fingerprint = computeFingerprint(path, rule, message);
+
+    expect(fingerprint).to.eq("dc015c73eb6381ef4c5e96774b0b460e");
+  });
+
   it("returns null for non-matching checks", function() {
     var path = "some/path.js"
       , rule = "nope"
