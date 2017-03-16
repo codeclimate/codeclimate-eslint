eb691655d337ac1d06d92af0a5beaf37ed15b1d6
Filipe Esperandio <filipesperandio@gmail.com>
2017-03-16 18:47:36 -0300

    Simplify the way to output result

--- bin/eslint.js
+++ bin/eslint.js
@@ -3,16 +3,7 @@
 const CODE_DIR = "/code";
 process.chdir(CODE_DIR);
 
-// Redirect `console.log` so that we are the only ones
-// writing to STDOUT
-var stdout = console.log;
-console.log = console.error;
-
-function print(msg) {
-  process.stdout.write(msg);
-}
-
 const ESLint = require("../lib/eslint");
-const exitCode = ESLint.run(print, { dir: CODE_DIR });
+const exitCode = ESLint.run({ dir: CODE_DIR });
 
 process.exit(exitCode);
--- integration/eslint_test.js
+++ integration/eslint_test.js
@@ -3,20 +3,36 @@ const expect = require("chai").expect;
 
 global.gc = function(){};
 
+const STDOUT = console.log;
+const STDERR = console.error;
+
 describe("eslint integration", function() {
   describe("eslintrc has not supported plugins", function() {
+    before(function() {
+      console.log = sinon.spy();
+      console.error = sinon.spy();
+    });
+
+    after(function() {
+      console.log = STDOUT;
+      console.error = STDERR;
+    });
+
     it("does not raise any error", function() {
       this.timeout(3000);
 
-      var print = sinon.spy();
+      var consoleStub = {
+        log: sinon.spy(),
+        error: sinon.spy()
+      };
 
       function execute() {
         const ESLint = require('../lib/eslint');
-        ESLint.run(print, { dir: __dirname, configPath: `${__dirname}/with_unsupported_plugins/config.json`});
+        ESLint.run({ dir: __dirname, configPath: `${__dirname}/with_unsupported_plugins/config.json`});
       }
 
       expect(execute).to.not.throw();
-      expect(print.called).to.be.ok;
+      expect(console.log.called).to.be.ok;
     });
   });
 
